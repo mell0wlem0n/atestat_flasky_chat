80537ae73bc3cd07e20983cd5ef68d5818a71c72
John Doe <118373838+mell0wlem0n@users.noreply.github.com>
2024-05-11 20:40:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,6 @@ class Config:
     FLASKY_MAIL_SUBJECT_PREFIX = "[Flasky]"
     FLASKY_MAIL_SENDER = "stoievlad11@gmail.com"
     FLASKY_ADMIN = os.environ.get("FLASKY_ADMIN")
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME", "stoievlad11@gmail.com")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD", "ntxpxquqhomsixii")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
     FLASKY_FRIENDS_PER_PAGE = 50
